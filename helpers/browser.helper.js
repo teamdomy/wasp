f7d4109d15cad0287c157d420c988d144113d7ae
Boris Siscanu <boris.siscanu@yahoo.com>
2018-08-23 14:02:37 +0300

    helper for wasp execution in the worker

---
+++ helpers/browser.helper.js
@@ -0,0 +1,25 @@
+/**
+ * Wrap the function from the wasp shell (worker execution)
+ *
+ * @param data
+ */
+export function wrap(data) {
+  return `
+    self.addEventListener('message', function(event) {
+      if (
+        event.data.order &&
+        event.data.order.data &&
+        event.data.order.data.hasOwnProperty('params')
+      ) {
+
+        var order = event.data.order;
+        order.data['value'] = {};
+
+        var result = (${data}).apply(null, order.data.params);
+
+        order.data.value = result;
+        self.postMessage({ order: order });
+      }
+    });
+  `;
+}
